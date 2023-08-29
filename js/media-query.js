ed012fff74da2c727695636acd9442c226014c3b
victor-losada <victorlosada2211@gmail.com>
2023-08-28 20:27:32 -0500

    holahp2

---
+++ js/media-query.js
@@ -0,0 +1,7 @@
+let logohamburguer = document.getElementById("logohamburguer");
+let mainMenu = document.getElementById("mainMenu")
+logohamburguer.addEventListener("click", function() {
+    
+    mainMenu.classList.add("main-menu-block")
+
+}) 
